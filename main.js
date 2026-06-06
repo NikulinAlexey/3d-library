2e9af138b79278fd5a8763b0e3040c4a70571aaa
Alsekey <donaldduckisnotdead@gmail.com>
2026-06-07 00:00:15 +0300

    Добавляет 2 новые модели: электронка и повербанк

--- main.js
+++ main.js
@@ -8,9 +8,21 @@ import { FBXLoader } from 'three/addons/loaders/FBXLoader.js';
 // ===== МАССИВ МОДЕЛЕЙ - ДОБАВЛЯЙТЕ МОДЕЛИ СЮДА =====
 const modelsList = [
   {
-    id: 1,
+    id: 0,
     name: "Мышка-v1",
     path: "./models/mouse-v1/model.glb",
+    format: "glb",
+  },
+  {
+    id: 1,
+    name: "Электронка-v1",
+    path: "./models/vape-v1/model.glb",
+    format: "glb",
+  },
+  {
+    id: 2,
+    name: "Повербанк-v1",
+    path: "./models/powerbank-v1/model.glb",
     format: "glb",
   },
   // ДОБАВЛЯЙТЕ НОВЫЕ МОДЕЛИ ПО ШАБЛОНУ:
