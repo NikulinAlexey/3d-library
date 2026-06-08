665429f94ae5ca5152170e2982e9da98e1dda1f8
Alsekey <donaldduckisnotdead@gmail.com>
2026-06-09 00:40:27 +0300

    Правит путь до main.js 5 раз

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
-// main.js
+const rootUrl = "3d-library/";
 import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 import { OBJLoader } from "three/addons/loaders/OBJLoader.js";
 import { FBXLoader } from "three/addons/loaders/FBXLoader.js";
-import { CONFIG } from "/js/config.js";
-import { setupLighting, setupHelpers } from "/js/sceneManager.js";
-import { loadModel, removeCurrentModel } from "/js/modelLoader.js";
+import { CONFIG } from `${rootUrl}/js/config.js`;
+import { setupLighting, setupHelpers } from `${rootUrl}/js/sceneManager.js`;
+import { loadModel, removeCurrentModel } from `${rootUrl}modelLoader.js`;
 import {
   initUI,
   showLoading,
   showError,
   updateCameraControls,
-} from "/js/uiController.js";
+} from `${rootUrl}/uiController.js`;
 
 // Глобальные переменные
 let scene, camera, renderer, controls;
