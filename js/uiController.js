3028a05bbe2ef65d1bd01301c1fc580a8f89a90b
Alsekey <donaldduckisnotdead@gmail.com>
2026-06-09 01:36:58 +0300

    Правит показ лоадера

--- js/uiController.js
+++ js/uiController.js
@@ -32,7 +32,10 @@ export function initUI(models, onSelectModel) {
 }
 
 export function showLoading(loadingIndicator, show) {
-  if (loadingIndicator) loadingIndicator.classList.add = "_visible";
+  if (loadingIndicator)
+    show
+      ? (loadingIndicator.classList.add = "_visible")
+      : (loadingIndicator.classList.remove = "_visible");
 }
 
 export function showError(errorDiv, message) {
